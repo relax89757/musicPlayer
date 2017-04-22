cff811775b356341e46e3680b9fb68190829d91e
relax <531113017@qq.com>
2017-04-22 19:05:09 +0800

    first commit

---
+++ js/music.js
@@ -0,0 +1,66 @@
+playlist = [
+      {
+      title: 'Lost Stars',
+      artist: 'Adam Levine',
+      album: 'Begin Again (Music From and Inspired By the Original Motion Picture)',
+      cover:'img/1.jpg',
+      mp3: 'mp3/Adam Levine - Lost Stars.mp3',
+      ogg: ''
+      },
+      {
+      title: 'A Thousand Years',
+      artist: 'Christina Perri',
+      album: 'A Thousand Years',
+      cover: 'img/2.jpg',
+      mp3: 'mp3/Christina Perri - A Thousand Years.mp3',
+      ogg: ''
+      },
+      {
+      title: '幸福特调',
+      artist: '罗志祥,裴秀智',
+      album: '真人秀？',
+      cover: 'img/3.jpg',
+      mp3: 'mp3/罗志祥,裴秀智 - 幸福特调.mp3',
+      ogg: ''
+      },
+      {
+      title: 'You And Me, Flutter',
+      artist: 'Acoustic Collabo ',
+      album: 'Unplugged',
+      cover: 'img/4.jpg',
+      mp3: 'mp3/Acoustic Collabo - You And Me, Flutter.mp3',
+      ogg: ''
+      },
+      {
+      title: '早安',
+      artist: '刘瑞琦',
+      album: '早安，琦',
+      cover: 'img/5.jpg',
+      mp3: 'mp3/刘瑞琦 - 早安.mp3',
+      ogg: ''
+      },
+      {
+      title: '云的舞蹈',
+      artist: '曹方',
+      album: 'WanderLust',
+      cover: 'img/6.jpg',
+      mp3: 'mp3/曹方 - 云的舞蹈.mp3',
+      ogg: ''
+      },
+      {
+      title: 'Counting Stars',
+      artist: 'OneRepublic',
+      album: 'Native (Deluxe Version)',
+      cover: 'img/7.jpg',
+      mp3: 'mp3/OneRepublic - Counting Stars.mp3',
+      ogg: ''
+      },
+      {
+      title: 'Color Blind',
+      artist: 'Matt B',
+      album: 'LOVE AND WAR',
+      cover: 'img/8.jpg',
+      mp3: 'mp3/Matt B - Color Blind.mp3',
+      ogg: ''
+      },
+      ];
